3e9f83b6e3784ec1a19f1e4e7f8f8f53ee08f35d
JohnnyTeutonic <jonathanreich100@gmail.com>
2017-08-06 18:05:09 +1000

    Memoize, conform to pep8

--- view_all.py
+++ view_all.py
@@ -6,7 +6,7 @@ from skimage import morphology as morph, measure
 from skimage.segmentation import mark_boundaries
 import sys
 import argparse
-
+from functools import wraps
 
 try:
     raw, gt = imio.read_cremi(sys.argv[1],  datasets=
@@ -23,7 +23,16 @@ gt = gt[gt.shape[0]//2]
 seeds = regular_seeds(raw.shape, np.random.randint(1100, 2100))
 automated_seg = morph.watershed(raw, seeds, compactness=0.001)
 
+def memo(func):
+    cache = {}
+    @wraps(func)
+    def wrap(*args):
+        if args not in cache:
+            cache[args] = func(*args)
+        return cache[args]
+    return wrap
 
+@memo
 def view_all(gt, automated_seg, num_elem=4):
         """Generates a click-able image of the auto seg - upon click of
         the auto seg, shows the largest comps of the gt that corresponds
@@ -48,64 +57,64 @@ def view_all(gt, automated_seg, num_elem=4):
             return "Input arrays are not of the same shape."
         elif (type(gt) or type(automated_seg)) != np.ndarray:
             return "Input arrays not of valid type."
-        else:
-            cont = ev.contingency_table(automated_seg, gt)
-            ii2, err2 = ev.sorted_vi_components(automated_seg, gt)[2:4]
-            idxs = np.argsort(ii2)
-            err_unsorted = err2[idxs]
-            err_img = err_unsorted[automated_seg]
-            plt.interactive = False
-            fig, ax = plt.subplots(nrows=2, ncols=2, sharex=True, sharey=True, figsize=(9,9))
-            plt.setp(ax.flat, aspect=1.0, adjustable='box-forced')
-            ax[0,0].imshow(raw, cmap='gray')
-            viz.imshow_rand(automated_seg, alpha=0.4, axis=ax[0, 0])
-            ax[0,1].imshow(raw, cmap='gray')
-            axes_image_1 = viz.imshow_rand(automated_seg, alpha=0.4, axis=ax[0,1])
-            ax[1,0].imshow(raw, cmap='gray')
-            viz.imshow_rand(gt, alpha=0.4, axis=ax[1, 0])
-            ax[1,1].imshow(raw, cmap='gray')
-            axes_image = viz.imshow_rand(gt, alpha=0.4, axis=ax[1, 1])
-            ax[0, 0].set_title("Automated seg: click to show worst splits.")
-            ax[0, 1].set_title("Worst merge comps in gt, colored by VI error.")
-            ax[1, 0].set_title("Ground truth with random cm: click to show worst merges.")
-            ax[1, 1].set_title("Worst split comps in the gt, colored by VI error.")
 
-            def _onpress(event):
-                if event.inaxes == ax[1, 0]:
-                    if event.button != 1:
-                        return
-                    x, y = int(event.xdata), int(event.ydata)
-                    comps = ev.split_components(gt[y, x], cont, axis=1, num_elems=None)
-                    new_seg = np.zeros_like(automated_seg)
-                    factor = (automated_seg.max() // num_elem)
-                    lim = 0.0
-                    for i, (j, k, z) in enumerate(comps):
-                        lim += k
-                        if z < 0.02:
-                            continue
-                        new_seg += (automated_seg == j) * ((i + 1) * factor)
-                        if lim >= 0.95:
-                            break
-                    axes_image.set_array(new_seg)
-                    fig.canvas.draw()
-                elif event.inaxes == ax[0, 0]:
-                    if event.button != 1:
-                        return
-                    x, y = int(event.xdata), int(event.ydata)
-                    comps = ev.split_components(automated_seg[y, x], cont, axis=0, num_elems=num_elem)
-                    new_seg = np.zeros_like(gt.astype('float64'))
-                    factor = (gt.max() // num_elem)
-                    lim = 0.0
-                    for i, (j,k, z) in enumerate(comps):
-                        lim +=k
-                        if z < 0.02:
-                            continue
-                        new_seg += (gt == j) * ((i + 1) * factor)
-                    axes_image_1.set_array(new_seg)
-                    fig.canvas.draw()
-            
-            fig.canvas.mpl_connect('button_press_event', _onpress)
-            plt.ioff()
-            plt.show()
+        cont = ev.contingency_table(automated_seg, gt)
+        ii2, err2 = ev.sorted_vi_components(automated_seg, gt)[2:4]
+        idxs = np.argsort(ii2)
+        err_unsorted = err2[idxs]
+        err_img = err_unsorted[automated_seg]
+        plt.interactive = False
+        fig, ax = plt.subplots(nrows=2, ncols=2, sharex=True, sharey=True, figsize=(9,9))
+        plt.setp(ax.flat, aspect=1.0, adjustable='box-forced')
+        ax[0, 0].imshow(raw, cmap='gray')
+        viz.imshow_rand(automated_seg, alpha=0.4, axis=ax[0, 0])
+        ax[0, 1].imshow(raw, cmap='gray')
+        axes_image_1 = viz.imshow_rand(automated_seg, alpha=0.4, axis=ax[0,1])
+        ax[1, 0].imshow(raw, cmap='gray')
+        viz.imshow_rand(gt, alpha=0.4, axis=ax[1, 0])
+        ax[1, 1].imshow(raw, cmap='gray')
+        axes_image = viz.imshow_rand(gt, alpha=0.4, axis=ax[1, 1])
+        ax[0, 0].set_title("Automated seg: click to show worst splits.")
+        ax[0, 1].set_title("Worst merge comps in gt, colored by VI error.")
+        ax[1, 0].set_title("Ground truth with random cm: click to show worst merges.")
+        ax[1, 1].set_title("Worst split comps in the gt, colored by VI error.")
+
+        def _onpress(event):
+            if event.inaxes == ax[1, 0]:
+                if event.button != 1:
+                    return
+                x, y = int(event.xdata), int(event.ydata)
+                comps = ev.split_components(gt[y, x], cont, axis=1, num_elems=None)
+                new_seg = np.zeros_like(automated_seg)
+                factor = (automated_seg.max() // num_elem)
+                lim = 0.0
+                for i, (j, k, z) in enumerate(comps):
+                    lim += k
+                    if z < 0.02:
+                        continue
+                    new_seg += (automated_seg == j) * ((i + 1) * factor)
+                    if lim >= 0.95:
+                        break
+                axes_image.set_array(new_seg)
+                fig.canvas.draw()
+            elif event.inaxes == ax[0, 0]:
+                if event.button != 1:
+                    return
+                x, y = int(event.xdata), int(event.ydata)
+                comps = ev.split_components(automated_seg[y, x], cont, axis=0, num_elems=num_elem)
+                new_seg = np.zeros_like(gt.astype('float64'))
+                factor = (gt.max() // num_elem)
+                lim = 0.0
+                for i, (j,k, z) in enumerate(comps):
+                    lim +=k
+                    if z < 0.02:
+                        continue
+                    new_seg += (gt == j) * ((i + 1) * factor)
+                axes_image_1.set_array(new_seg)
+                fig.canvas.draw()
+
+        fig.canvas.mpl_connect('button_press_event', _onpress)
+        plt.ioff()
+        plt.show()
 
 view_all(gt, automated_seg)
