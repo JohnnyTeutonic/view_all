b2e0b29dabefe55081d982378b9c237142789438
JohnnyTeutonic <jonathanreich100@gmail.com>
2017-08-08 12:44:17 +1000

    Include new files for project

---
+++ __init__.py
@@ -0,0 +1,12 @@
+"""
+View_All
+===
+
+    View_All is a Python package for nD image segmentation.
+"""
+
+__author__ = 'Jonathan Reich <jreich@student.unimelb.edu.au>, '
+
+__all__ = ['view_all']
+
+__version__ = '0.1dev'
