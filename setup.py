2263c5d699e346df92031f344479e9963e7f909e
JohnnyTeutonic <Jonathanreich100@gmail.com>
2017-08-17 23:28:02 +1000

    Modified setup.py

--- setup.py
+++ setup.py
@@ -1,27 +1,18 @@
-from distutils.core import setup
+from distutils.core import setup, find_packages
 
 files = ["view_all.py","clickable_image_new.py"]
 
 setup(name = "view_all",
-    version = "0.1a",
-    description = "yadda yadda",
+    version = "0.1-dev",
+    description = "A 3D Viewer for neuronal volumetric data.",
     author = "Jonathan Reich",
     author_email = "jreich@student.unimelb.edu.au",
     url = "https://github.com/JohnnyTeutonic/view_all"
+    install_requires=['numpy', 'numba', 'gala', 'matplotlib']
     #Name the folder where your packages live:
-    #(If you have other packages (dirs) or modules (py files) then
-    #put them into the package directory - they will be found 
-    #recursively.)
-    packages = ['package'],
-    #'package' package must contain files (see list above)
-    #I called the package 'package' thus cleverly confusing the whole issue...
-    #This dict maps the package name =to=> directories
-    #It says, package *needs* these files.
-    package_data = {'package' : files },
-    #'runner' is in the root.
-    scripts = ["runner"],
-    long_description = """Really long text here.""" 
-    #
-    #This next part it for the Cheese Shop, look a little down the page.
-    #classifiers = []
+    #If you have other packages (dirs) or modules (py files) then
+    #put them into the package directory - they will be found
+    #recursively."
+    packages = [find_packages()]
+    scripts = ["runner"]
 )
