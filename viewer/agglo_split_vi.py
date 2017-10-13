6b65696d75a858f5d1d01dfb23791c3f37891ca8
JohnnyTeutonic <jonathanreich100@gmail.com>
2017-10-13 13:50:48 +1100

    Include bpm

--- viewer/agglo_split_vi.py
+++ viewer/agglo_split_vi.py
@@ -4,25 +4,29 @@ from skimage import io
 import numpy as np
 from matplotlib import pyplot as plt
 raw, gt = imio.read_cremi("Cremi_Data/sample_B_20160501.hdf", datasets=['volumes/raw', 'volumes/labels/neuron_ids'])
-gt_larger_2 = gt[0:15, 0:480, 0:480]
-raw_larger_2 = 1-raw[0:15, 0:480, 0:480]/255
+bpm = imio.read_h5_stack('raw_slice_1_Probabilities.h5', group='bpm_raw_b')
+membrane_prob = bpm[..., 2]
+train_slice = (slice(0, 15), slice(0, 480), slice(0, 480))
+test_slice = (slice(0, 15), slice(480, 960), slice(480, 960))
+gt_larger_2 = gt[train_slice]
+raw_larger_2 = 1-raw[train_slice]/255
 ws_larger_seeds_2 = regular_seeds(raw_larger_2[0].shape, n_points=700)
 ws_larger_seeds_2 = np.broadcast_to(ws_larger_seeds_2, raw_larger_2.shape)
-ws_larger_water = morpho.watershed_sequence(raw_larger_2, ws_larger_seeds_2, n_jobs=-1)
-raw_larger_testing_2 = 1-raw[0:15, 480:960, 480:960]/255
-gt_larger_testing_2 = gt[0:15, 480:960, 480:960]
+ws_larger_water = morpho.watershed_sequence(membrane_prob[train_slice], ws_larger_seeds_2, n_jobs=-1)
+raw_larger_testing_2 = membrane_prob[test_slice]
+gt_larger_testing_2 = gt[test_slice]
 gg = np.argsort(np.bincount(gt_larger_2.astype(int).ravel()))[-10:]
-sparse_large = imio.extract_segments(raw_larger_2, ids = gg)
+sparse_large = imio.extract_segments(gt_larger_2, ids = gg)
 ws_larger_testing_2 = morpho.watershed_sequence(raw_larger_testing_2, ws_larger_seeds_2, n_jobs=-1)
 fm = features.moments.Manager()
 fh = features.histogram.Manager()
 fc = features.base.Composite(children=[fm, fh])
-g_train_larger_2 = agglo.Rag(ws_larger_water, raw_larger_2, feature_manager=fc)
+g_train_larger_2 = agglo.Rag(ws_larger_water, bpm[train_slice], feature_manager=fc)
 (X2, y2, w2, merges2) = g_train_larger_2.learn_agglomerate(gt_larger_2, fc, classifier='logistic')[0]
 y2 = y2[:, 0]
 rf_log_large_2 = classify.get_classifier('logistic').fit(X2,y2)
 learned_policy_large_2 = agglo.classifier_probability(fc, rf_log_large_2)
-g_test_large_2 = agglo.Rag(ws_larger_testing_2, raw_larger_testing_2, feature_manager=fc, merge_priority_function=learned_policy_large_2)
+g_test_large_2 = agglo.Rag(ws_larger_testing_2, bpm[test_slice], feature_manager=fc, merge_priority_function=learned_policy_large_2)
 g_test_large_2.agglomerate(np.inf)
 seg_stack_large_2 = [g_test_large_2.get_segmentation(t) for t in np.arange(0,1, 0.01)]
 split_vi_score_large_2 = [ev.split_vi(seg_stack_large_2[t], gt_larger_testing_2) for t in range(len(seg_stack_large_2))]
