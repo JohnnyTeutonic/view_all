3e5d145cb5b7e0cb70ebc173f7acebc3f6c3e6ba
JohnnyTeutonic <jonathanreich100@gmail.com>
2017-11-14 01:15:56 +1100

    Remove redundant file

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from distutils.core import setup, find_packages
 
-files = ["view_all.py","clickable_image.py"]
+files = ["view_all.py", "view_all_orig.py", "clickable_image.py",
+         "segmentation_pipline.py"]
 
 setup(name = "view_all",
     version = "0.1-dev",
-    description = "A 3D Viewer for neuronal volumetric data.",
+    description = "A 3D Viewer for volumetric neuronal data.",
     author = "Jonathan Reich",
     author_email = "jreich@student.unimelb.edu.au",
     url = "https://github.com/JohnnyTeutonic/view_all",
