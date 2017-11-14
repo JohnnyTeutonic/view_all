977efd199bc4c30b16c718a8f2726377805a4c4c
JohnnyTeutonic <jonathanreich100@gmail.com>
2017-11-14 20:08:56 +1100

    Refinement of functions

--- viewer/segmentation_pipeline.py
+++ viewer/segmentation_pipeline.py
@@ -9,6 +9,7 @@ import multiprocessing as mp
 # libraries
 from gala import evaluate as ev, imio, viz, morpho, agglo, classify, features
 import line_profiler as lp
+from skimage.measure import label
 from skimage.segmentation import join_segmentations
 from skimage.util import regular_seeds
 from skimage import io
@@ -20,20 +21,20 @@ os.chdir("/home/johnnyt/Documents/research_project_files/")
 raw, gt = imio.read_cremi("Cremi_Data/sample_B_20160501.hdf", datasets=['volumes/raw', 'volumes/labels/neuron_ids'])
 # Read in the boundary probability map for the volume_seg_bpm function
 bpm = imio.read_h5_stack('raw_slice_1_Probabilities.h5', group='bpm_raw_b')
-train_slice = (slice(15, 20), slice(0, 480), slice(0, 480))
-test_slice = (slice(15, 20), slice(480, 960), slice(480, 960))
+train_slice = (slice(15, 23), slice(0, 600), slice(600, 1200))
+test_slice = (slice(15, 23), slice(600, 1200), slice(600, 1200))
 gt_training = gt[train_slice]
 gt_testing = gt[test_slice]
 raw_training = 1 - raw[train_slice] / 255
 raw_testing = raw[test_slice]
 
 
-def volume_seg_bpm():
+def volume_seg_bpm(plot=False):
     """Perform a segmentation on a volume using the GALA algorithm and a boundary probability map."""
 
     membrane_prob = bpm[..., 2]
     raw_bpm_testing = membrane_prob[test_slice]
-    ws_larger_seeds = regular_seeds(raw_training[0].shape, n_points=500)
+    ws_larger_seeds = regular_seeds(raw_training[0].shape, n_points=300)
     ws_larger_seeds = np.broadcast_to(ws_larger_seeds, raw_training.shape)
     # Training step using membrane probability map
     ws_larger_train = morpho.watershed_sequence(membrane_prob[train_slice], ws_larger_seeds, n_jobs=-1)
@@ -61,30 +62,34 @@ def volume_seg_bpm():
     best_seg_ind_bpm = np.argmin(split_vi_array_bpm.sum(axis=1))
     best_seg_bpm = seg_stack_large_bpm[best_seg_ind_bpm]
     joint_seg = join_segmentations(best_seg_bpm, gt_testing)
-    fig, ax = plt.subplots(nrows=2, ncols=3, figsize=(20, 10))
-    ax[0, 0].set_title('Automatic Segmentation')
-    ax[0, 1].set_title('Join Segmentation')
-    ax[0, 2].set_title('Ground Truth')
-    ax[1, 2].set_title('Raw Data')
-    viz.imshow_rand(best_seg_bpm[3, ...], axis=ax[0, 0])
-    ax[0, 0].axis('off')
-    viz.imshow_rand(joint_seg[3, ...], axis=ax[0, 1])
-    ax[0, 1].axis('off')
-    viz.imshow_rand(gt_testing[3, ...], axis=ax[0, 2])
-    ax[0, 2].axis('off')
-    ax[1, 0].plot(split_vi_array_bpm[:, 1], split_vi_array_bpm[:, 0])
-    ax[1, 0].set_xlabel('False merges')
-    ax[1, 0].set_ylabel('False splits')
-    ax[1, 0].set_title("Split VI")
-    ax[1, 1].set_title("Cumulative VI")
-    ax[1, 1].plot(split_vi_array_bpm.sum(axis=1))
-    ax[1, 2].imshow(raw_testing[3,...])
-    plt.show()
+    if plot:
+        fig, ax = plt.subplots(nrows=2, ncols=3, figsize=(20, 10))
+        ax[0, 0].set_title('Automatic Segmentation')
+        ax[0, 1].set_title('Join Segmentation')
+        ax[0, 2].set_title('Ground Truth')
+        ax[1, 2].set_title('Raw Data')
+        viz.imshow_rand(best_seg_bpm[3, ...], axis=ax[0, 0])
+        ax[0, 0].axis('off')
+        viz.imshow_rand(joint_seg[3, ...], axis=ax[0, 1])
+        ax[0, 1].axis('off')
+        viz.imshow_rand(gt_testing[3, ...], axis=ax[0, 2])
+        ax[0, 2].axis('off')
+        ax[1, 0].plot(split_vi_array_bpm[:, 1], split_vi_array_bpm[:, 0])
+        ax[1, 0].set_xlabel('False merges (in bits)')
+        ax[1, 0].set_ylabel('False splits (in bits)')
+        ax[1, 0].set_title("Split VI")
+        ax[1, 1].set_title("Cumulative VI")
+        ax[1, 1].set_xlabel('Merge Threshold')
+        ax[1, 1].set_ylabel('VI (in bits)')
+        ax[1, 1].plot(split_vi_array_bpm.sum(axis=1))
+        ax[1, 2].imshow(raw_training[3,...])
+        plt.show()
+    return np.min(split_vi_array_bpm.sum(axis=1))
 
 
-def volume_seg_non_bpm():
+def volume_seg_non_bpm(plot=False):
     """Perform a segmentation on a volume using the GALA algorithm and the raw data as input into the watershed map."""
-   
+
     ws_larger_seeds = regular_seeds(raw_training[0].shape, n_points=300)
     ws_larger_seeds = np.broadcast_to(ws_larger_seeds, raw_training.shape)
     # Training step using output from watershedding step
@@ -112,24 +117,29 @@ def volume_seg_non_bpm():
     best_seg_ind = np.argmin(split_vi_array.sum(axis=1))
     best_seg = seg_stack_large[best_seg_ind]
     joint_seg = join_segmentations(best_seg, gt_testing)
-    fig, ax = plt.subplots(nrows=2, ncols=3, figsize=(20, 10))
-    ax[0, 0].set_title('Automatic Segmentation')
-    ax[0, 1].set_title('Join Segmentation')
-    ax[0, 2].set_title('Ground Truth')
-    viz.imshow_rand(best_seg[3, ...], axis=ax[0, 0])
-    ax[0, 0].axis('off')
-    viz.imshow_rand(joint_seg[3, ...], axis=ax[0, 1])
-    ax[0, 1].axis('off')
-    viz.imshow_rand(gt_training[3, ...], axis=ax[0, 2])
-    ax[0, 2].axis('off')
-    ax[1, 0].plot(split_vi_array[:, 1], split_vi_array[:, 0])
-    ax[1, 0].set_xlabel('False merges')
-    ax[1, 0].set_ylabel('False splits')
-    ax[1, 0].set_title("Split VI")
-    ax[1, 1].set_title("Cumulative VI")
-    ax[1, 1].plot(split_vi_array.sum(axis=1))
-    ax[1, 2].imshow(raw_testing[3, ...])
-    plt.show()
+    if plot:
+        fig, ax = plt.subplots(nrows=2, ncols=3, figsize=(20, 10))
+        ax[0, 0].set_title('Automatic Segmentation')
+        ax[0, 1].set_title('Join Segmentation')
+        ax[0, 2].set_title('Ground Truth')
+        viz.imshow_rand(best_seg[3, ...], axis=ax[0, 0])
+        ax[0, 0].axis('off')
+        viz.imshow_rand(joint_seg[3, ...], axis=ax[0, 1])
+        ax[0, 1].axis('off')
+        viz.imshow_rand(gt_training[3, ...], axis=ax[0, 2])
+        ax[0, 2].axis('off')
+        ax[1, 0].plot(split_vi_array[:, 1], split_vi_array[:, 0])
+        ax[1, 0].set_xlabel('False merges (in bits)')
+        ax[1, 0].set_ylabel('False splits (in bits)')
+        ax[1, 0].set_title("Split VI")
+        ax[1, 1].set_title("Cumulative VI")
+        ax[1, 1].set_xlabel('Merge Threshold')
+        ax[1, 1].set_ylabel('VI (in bits)')
+        ax[1, 1].plot(split_vi_array.sum(axis=1))
+        ax[1, 2].imshow(raw_training[3, ...])
+        plt.show()
+    return np.min(split_vi_array.sum(axis=1))
+
 
 def write_out():
     '''Write out the worst false split comps, worst false merge comps and sparse versions of the ground truth
@@ -171,5 +181,10 @@ def write_out():
     imio.write_vtk(extracted_worst_merge_comps_m, fn=f'worst_merge_comps_m_{short_date}.vtk', spacing=spacing)
     imio.write_vtk(extracted_worst_split_comps_s, fn=f'worst_split_comps_s_{short_date}.vtk', spacing=spacing)
 
+
 if __name__ == '__main__':
-    volume_seg_bpm()
+
+    minimum_bpm = volume_seg_bpm(plot=True)
+    #minimum_non_bpm = volume_seg_non_bpm(plot=True)
+    #print(f"The minimum Cumulative VI of the non_bpm segmentation is {minimum_non_bpm}\n")
+    print(f"The minimum Cumulative VI of the bpm segmentation is {minimum_bpm}\n")
